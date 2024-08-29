e8940600c55a5bf50631b34012edd7b1753d5555
An0create <an0creates@gmail.com>
2024-08-29 11:35:56 -0500

    Added comprehensions.py

--- file_ops.py
+++ file_ops.py
@@ -1,39 +1,35 @@
 def read_file(file_name):
-    f = open('sampletext.txt')
-    print(f.read())
-    raise NotImplementedError()
-    
+    with open(file_name, 'r') as f:
+        contents = f.read()
+        print(contents)
+        return contents
 
 def read_file_into_list(file_name):
-    f = open('sampletext.txt', 'r')
-    print(f.readlines())
-    raise NotImplementedError()
+    with open(file_name, 'r') as f:
+        contents = f.readlines()
+        return contents
 
 def write_first_line_to_file(file_contents, output_filename):
-    f = open('sampletext.txt')
-    print(f.readline())
-    raise NotImplementedError()
-
+    with open(output_filename, 'w') as f:
+        f.write(file_contents.split('\n')[0])
 
 def read_even_numbered_lines(file_name):
-    f = open('sampletext.txt', 'r')
-    return list(f)[1:2]
-
-    raise NotImplementedError()
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        return [line.strip() for idx, line in enumerate(lines) if idx % 2 == 1]
 
 def read_file_in_reverse(file_name):
-    for line in reversed(list(open('sampletext.txt'))):
-        print(line.rstrip())
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        rev_lines = list(reversed(lines))
+        print(rev_lines)
+        return rev_lines
 
-    raise NotImplementedError()
-
-
+'''
+Here are some sample commands to help you run/test your implementations.
+Feel free to uncomment/modify/add to them as you wish.
+'''
 def main():
     file_contents = read_file("sampletext.txt")
     print(read_file_into_list("sampletext.txt"))
     write_first_line_to_file(file_contents, "online.txt")
-    print(read_even_numbered_lines("sampletext.txt"))
-    print(read_file_in_reverse("sampletext.txt"))
-
-if __name__ == "__main__":
-    main()
